d12f862fcbc66be2384a9c2b18a987e30939648d
sarthaksavvy <sarthak@bitfumes.com>
2019-07-01 10:41:24 +0530

    1. Intro to next.js

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const About = () => (
+    <div>
+        <Link href="/"><a>Home</a></Link>
+        <Link href="/about"><a>About</a></Link>
+        <h1>I am About Page</h1>
+    </div>
+)
+export default About
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Index = () => (
+    <div>
+        <Link href="/"><a>Home</a></Link>
+        <Link href="/about"><a>About</a></Link>
+        <h1>I am Home Page</h1>
+    </div>
+    )
+
+export default Index
